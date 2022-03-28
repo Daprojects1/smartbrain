8117327886e59f3237e1d01002df938e5f472ea0
Daprojects1 <davidrtm45@gmail.com>
2022-03-28 16:38:32 +0100

    errors due to non compatibility and outdated files

--- src/App.js
+++ src/App.js
@@ -1,112 +1,43 @@
-import './App.css';
+import React from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from "./Components/Logo/logo"
 import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-tsparticles';
+import particlesConfig from "./ParticlesFiles/particles"
+import './App.css';
 
-function App() {
-  const particlesInit = (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-  const particlesOptions = {
-
-    fpsLimit: 160,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        resize: true,
-      },
-      modes: {
-        bubble: {
-          distance: 400,
-          duration: 2,
-          opacity: 0.8,
-          size: 40,
-        },
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outMode: "bounce",
-        random: false,
-        speed: 2,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 120,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        random: true,
-        value: 5,
-      },
-    },
-    detectRetina: true,
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      input: "",
+    }
   }
-
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-  return (
-    <div className='App'>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {
-        //   <FaceRecognition/>
-      }
-    </div>
-  );
+  onInputChange = (e) => {
+  }
+  onClickDetectBtn = (e) => {
+    console.log(e)
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Particles
+          id="tsparticles"
+          init={particlesConfig.particlesInit}
+          loaded={particlesConfig.particlesLoaded}
+          options={particlesConfig.particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onClickDetectBtn={this.onClickDetectBtn} />
+        {
+          //   <FaceRecognition/>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import "./imagelink.css"
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onClickDetectBtn }) => {
     return (
         <div>
             <p className="f3 ma3 tc">{"This magic brain will detect faces. Give it a try"}</p>
             <div className="center">
                 <div className="pa4 br3 form center shadow-5">
-                    <input className="f4 pa2 w-70 center" type="text" />
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                    <input className="f4 pa2 w-70 center" onChange={onInputChange} type="text" />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onClickDetectBtn}>Detect</button>
                 </div>
             </div>
         </div>
